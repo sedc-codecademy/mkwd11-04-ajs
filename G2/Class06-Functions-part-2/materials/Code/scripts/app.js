890bfd5a2c002f2b0e70db03e2069f1874e3b0b5
Bojan Damchevski <bod@proofficepark.dk>
2023-01-26 20:29:38 +0100

    Code class 06

--- G2/Class06-Functions-part-2/materials/Code/scripts/app.js
+++ G2/Class06-Functions-part-2/materials/Code/scripts/app.js
@@ -27,3 +27,118 @@ let students2 = [
     age: 33,
   },
 ];
+
+//Without higher order functions
+// for (let i = 0; i < students.length; i++) {
+//   console.log(`${students[i].firstName} ${students[i].lastName}`);
+// }
+
+//With higher order functions
+// function logFullName(student) {
+//   console.log(`${student.firstName} ${student.lastName}`);
+// }
+
+// students.forEach(logFullName);
+
+// With higher order functions but with an anonymous function
+// students.forEach(function (student) {
+//   console.log(`${student.firstName} ${student.lastName}`);
+// });
+
+// With higher order functions but with an arrow function
+// students.forEach((student) =>
+//   console.log(`${student.firstName} ${student.lastName}`)
+// );
+
+// Complex use #1
+students2.forEach((s) =>
+  s.grades
+    .filter((g) => g.grade === 5)
+    .forEach((g) => console.log(`${g.subject}: ${g.grade}`))
+);
+
+//Filter
+let above18 = [];
+for (let student of students) {
+  if (student.age >= 18 && student.grade === 5) {
+    above18.push(student);
+  }
+}
+
+console.log(above18);
+
+let studentsAbove18 = students.filter((s) => s.age >= 18);
+
+console.log(studentsAbove18);
+
+let studentsAbove18WithGradeEqual5 = studentsAbove18.filter(
+  (s) => s.grade === 5
+);
+
+let studentsAbove18WithGrade5Short = students
+  .filter((s) => s.age >= 18)
+  .filter((s) => s.grade === 5);
+
+let studentsAbove18WithGrade5EvenShorter = students.filter(
+  (s) => s.age >= 18 && s.grade === 5
+);
+
+// Map
+let studentFirstNames = students.map((s) => s.firstName);
+
+// console.log(studentFirstNames);
+
+let names = students.map((s) => {
+  return {
+    name: `${s.firstName} ${s.lastName}`,
+    grade: s.grade,
+  };
+});
+
+let namesSimplified = students.map((s) => `${s.firstName} ${s.lastName}`);
+
+console.log(namesSimplified);
+
+let studentsUnder18 = students.filter((s) => s.age < 18);
+
+console.log(studentsUnder18);
+
+let studentsAge = studentsUnder18.map((s) => s.age);
+
+console.log(studentsAge);
+
+let studentsUnder18Short = students
+  .filter((s) => s.age < 18)
+  .map((s) => s.age + " no");
+
+console.log(studentsUnder18Short);
+
+// Reduces
+let sumOfAllGrades = students
+  .map((s) => s.grade)
+  .reduce((sum, grade) => (sum += grade), 0);
+
+console.log(sumOfAllGrades);
+
+let allNamesString = students
+  .map((s) => s.firstName)
+  .reduce((bigString, name) => (bigString += ` ${name},`), "Names: ");
+
+console.log(allNamesString);
+
+let allGradesExceptLowest = students
+  .filter((s) => s.grade > 1)
+  .map((s) => s.grade)
+  .reduce((sum, grade) => (sum += grade), 0);
+
+console.log(allGradesExceptLowest);
+
+// Spread and sort
+let allGrades = students.map((s) => s.grade);
+
+let sortedGradesAsc = allGrades.sort((a, b) => a - b);
+let allGradesCopy = [...allGrades]
+let sortedGradesDesc = allGradesCopy.sort((a, b) => b - a);
+console.log(allGrades);
+console.log(sortedGradesAsc)
+console.log(sortedGradesDesc)
