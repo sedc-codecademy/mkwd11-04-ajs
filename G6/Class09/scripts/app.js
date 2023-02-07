2f337901822ceb8a32423890a638653895073bca
ivokostovski <ivo.kostovski@gmail.com>
2023-02-07 19:39:51 +0100

    class 9 - break 2

--- G6/Class09/scripts/app.js
+++ G6/Class09/scripts/app.js
@@ -41,23 +41,166 @@
 // 1. Pending
 // 2. Fulfilled / Rejected
 
-const first = (isOk) => {
+// const first = (isOk) => {
+//     return new Promise((resolve, reject) => {
+//         if (isOk) {
+//             resolve('ok')
+//         } else {
+//             reject('not ok')
+//         }
+//     })
+// }
+
+// first(true)
+//     .then(data => {
+//         console.log(data)
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+//     .finally(() => {
+//         console.log('The end')
+//     })
+
+//https://raw.githubusercontent.com/sedc-codecademy/mkwd11-04-ajs/main/G6/Class09/data/documents.json
+
+// Example 1
+
+// const getDocuments = () => {
+//     return new Promise((resolve, reject) => {
+//         fetch(`https://raw.githubusercontent.com/sedc-codecademy/mkwd11-04-ajs/main/G6/Class09/data/documents.json`)
+//             .then(response => {
+//                 if (response.ok) {
+//                     return response.json()
+//                 } else {
+//                     reject('Documents fetching failed.')
+//                     // throw new Error('')
+//                 }                
+//             })
+//             .then(documents => {
+//                 resolve(documents)
+//             })
+//             .catch(error => {
+//                 reject(error)
+//             })
+//     })
+// }
+
+// const showDocuments = (documents) => {
+//     if (!documents || !Array.isArray(documents)) {
+//         throw new Error('There is a problem with the documents.')
+//     }
+
+//     if (!documents.length) {
+//         throw new Error('There are no documents.')
+//     }
+
+//     documents.forEach(document => {
+//         console.log(`${document.name}.${document.type} ${document.size}MB`)
+//     });
+// }
+
+// getDocuments()
+//     .then(documents => {
+//         showDocuments(documents)
+//     })
+//     .catch(error => console.log(error))
+
+
+// Example 2
+
+// const getDocuments = () => {
+//     return new Promise((resolve, reject) => {
+//         fetch(`https://raw.githubusercontent.com/sedc-codecademy/mkwd11-04-ajs/main/G6/Class09/data/documents.json`)
+//             .then(response => {
+//                 if (response.ok) {
+//                     return response.json()
+//                 } else {
+//                     reject('Documents fetching failed.')
+//                 }                
+//             })
+//             .then(documents => {
+//                 resolve(documents)
+//             })
+//             .catch(error => {
+//                 reject(error)
+//             })
+//     })
+// }
+
+// const showDocuments = (documents) => {
+//     documents.forEach(document => {
+//         console.log(`${document.name}.${document.type} ${document.size}MB`)
+//     });
+// }
+
+// const getImportantDocuments = (documents) => {
+//     return documents.filter(document => document.important)
+// }
+
+// const checkDocuments = (documents) => {
+//     if (!documents || !Array.isArray(documents)) {
+//         throw new Error('There is a problem with the documents.')
+//     }
+
+//     if (!documents.length) {
+//         throw new Error('There are no documents.')
+//     }
+// }
+
+// getDocuments()
+//     .then(documents => {
+//         checkDocuments(documents)
+//         const importantDocuments = getImportantDocuments(documents)
+//         showDocuments(importantDocuments)
+//     })
+//     .catch(error => console.log(error))
+
+// Async / Await
+
+const getDocuments = () => {
     return new Promise((resolve, reject) => {
-        if (isOk) {
-            resolve('ok')
-        } else {
-            reject('not ok')
-        }
+        fetch(`https://raw.githubusercontent.com/sedc-codecademy/mkwd11-04-ajs/main/G6/Class09/data/documents.json`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    reject('Documents fetching failed.')
+                }                
+            })
+            .then(documents => {
+                resolve(documents)
+            })
+            .catch(error => {
+                reject(error)
+            })
     })
 }
 
-first(true)
-    .then(data => {
-        console.log(data)
+const showDocuments = (documents) => {
+    documents.forEach(document => {
+        console.log(`${document.name}.${document.type} ${document.size}MB`)
+    });
+}
+
+const getImportantDocuments = (documents) => {
+    return documents.filter(document => document.important)
+}
+
+const checkDocuments = (documents) => {
+    if (!documents || !Array.isArray(documents)) {
+        throw new Error('There is a problem with the documents.')
+    }
+
+    if (!documents.length) {
+        throw new Error('There are no documents.')
+    }
+}
+
+getDocuments()
+    .then(documents => {
+        checkDocuments(documents)
+        const importantDocuments = getImportantDocuments(documents)
+        showDocuments(importantDocuments)
     })
-    .catch(error => {
-        console.log(error)
-    })
-    .finally(() => {
-        console.log('The end')
-    })
+    .catch(error => console.log(error))
