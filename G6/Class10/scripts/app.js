4b7f7818099a279a02952c1c01e41af9e3fff015
ivokostovski <ivo.kostovski@gmail.com>
2023-02-09 18:34:43 +0100

    class 10 - break 1

--- G6/Class10/scripts/app.js
+++ G6/Class10/scripts/app.js
@@ -1,1 +1,76 @@
-console.log('Hello world!')
+// Everything in JS in an Object
+
+// console.log(typeof window)
+// console.log(typeof document)
+// console.log(typeof window.document)
+// console.log(typeof {})
+// console.log(typeof [])
+// console.log(typeof 'ivo kostovski')
+// console.log(typeof 'ivo kostovski'.split(''))
+
+// Shorthand method calls
+// window.console.log('test')
+// console.log('window', window)
+// console.log('console', window.console)
+// console.log()
+// console.error()
+// console.table()
+// alert('test')
+// window.alert('test 2')
+
+// Global scope
+
+// function sayHello() {
+//     console.log('hello')
+// }
+// sayHello()
+// window.sayHello()
+
+// let letVariable = 'let Variable';
+// const constVariable = 'const variable'
+// var varVariable = 'var variable'
+
+// console.log(letVariable)
+// console.log(window.letVariable)
+// console.log(window.constVariable)
+// console.log(window.varVariable)
+
+// Constructor functions
+
+// function Dog(name, color, age, favoriteFood, ownerName, city) {
+//     this.name = name;
+//     this.color = color;
+//     this.age = age;
+//     this.favoriteFood = favoriteFood;
+//     this.ownerName = ownerName;
+//     this.city = city;
+// }
+
+// Dog.favoriteToy = 'Ball'
+
+// let murdzo = new Dog('Murdzo', 'Bela', 5, 'Meso', 'Ivo', 'Skopje')
+
+// let blacky = new Dog('Blacky', 'black', 3, 'meat', 'Ivo', 'Skopje')
+
+// let emptyDog = new Dog()
+
+// let largeDog = new Dog('Large', 'yellow', 2, 'meat', 'Ivo', 'Skopje', 'Ball')
+
+// console.log(murdzo)
+
+// blacky.favoriteToy = 'Ball'
+
+// console.log(Dog.favoriteToy)
+
+// console.log(blacky)
+// console.log(emptyDog)
+// console.log(largeDog)
+
+function Dog(name, color, age, favoriteFood, ownerName, city) {
+    this.name = name;
+    this.color = color;
+    this.age = age;
+    this.favoriteFood = favoriteFood;
+    this.ownerName = ownerName;
+    this.city = city;
+}
