c44116e98e93b3f9ad7c53696cf1ec9119cbd7ee
ivokostovski <ivo.kostovski@gmail.com>
2023-02-09 19:43:25 +0100

    Class 10 break 2

--- G6/Class10/scripts/app.js
+++ G6/Class10/scripts/app.js
@@ -66,11 +66,84 @@
 // console.log(emptyDog)
 // console.log(largeDog)
 
-function Dog(name, color, age, favoriteFood, ownerName, city) {
+function Dog(name = 'N/A', color, age, favoriteFood, ownerName, city) {
     this.name = name;
-    this.color = color;
-    this.age = age;
+    this.color = color || 'Colorful';
+    this.age = age ?? 'N/A';
     this.favoriteFood = favoriteFood;
     this.ownerName = ownerName;
+    this.hasOwner = !!ownerName;
     this.city = city;
+
+    this.bark = function() {
+        console.log('Av Av')
+    }
 }
+
+// let noNameDog = new Dog()
+// console.log(noNameDog)
+
+// let puppy = new Dog('Blacky', 'White', 0)
+// console.log(puppy)
+
+// let mike = new Dog('Mikey', 'Brown', 2, 'meat', 'Ivo')
+
+// console.log(mike)
+
+// noNameDog.bark()
+// mike.bark()
+
+// THIS
+// window.surname = 'window obj surname';
+// window.firstName = 'window obj firstName';
+// window.whatIsThis = 'window obj this value'
+
+// let thisObj = {
+//     firstName: 'this obj name',
+//     whatIsThis: this.surname, // references to the Window object
+//     whatIsThisMethod: function() {
+//         console.log('Inside function this', this.firstName);
+//         console.log('global this', globalThis)
+//     },
+//     whatIsThisArrowMethod: () => {
+//         console.log('Inside arrow function this', this.firstName)
+//     }
+// }
+
+// console.log(thisObj.whatIsThis)
+
+// thisObj.whatIsThisMethod()
+// thisObj.whatIsThisArrowMethod()
+
+// Destructuring
+
+// const blacky = new Dog('Blacky', null, null, 'meat', 'Kristijan', 'Tetovo');
+
+// console.log(blacky)
+
+// console.log(blacky.name)
+
+// // const { name, age } = blacky;
+
+// const name = blacky.name
+// const age = blacky.age
+
+// console.log(name, age)
+
+// const color = 'yellow';
+
+// const { age = 'N/A', color: dogColor } = blacky
+
+// console.log(age, dogColor)
+
+
+// const rgb = [255, 200, 100];
+
+// const green = rgb[1]
+
+// const [ red, green, blue ] = rgb
+// const [ , , blue ] = rgb
+
+// console.log('red', 'green', 'blue')
+// console.log(red, green, blue)
+
