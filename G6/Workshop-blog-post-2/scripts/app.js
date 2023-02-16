3ef336fd780b828da8005dc95367226272e3068c
ivokostovski <ivo.kostovski@gmail.com>
2023-02-16 20:54:43 +0100

    blog post part 2 done

--- G6/Workshop-blog-post-2/scripts/app.js
+++ G6/Workshop-blog-post-2/scripts/app.js
@@ -24,6 +24,8 @@ const errorEl = document.getElementById('error')
 // DATA
 const baseUrl = 'https://jsonplaceholder.typicode.com';
 let allPosts = [];
+let isEditing = false;
+let editedPostId = null;
 
 // FUNCTIONS
 
@@ -46,18 +48,29 @@ const renderPosts = (posts) => {
                   <p class="card-text">
                     ${post.body}
                   </p>
+                  <a href="#" class="btn btn-warning" id="comments-edit-${post.id}">Edit</a>
+                  <a href="#" class="btn btn-danger" id="comments-delete-${post.id}">Delete</a>
                 </div>
-                <a href="#" class="btn btn-warning" id="comments-edit-${post.id}">Edit</a>
               </div>
             </div>
         `        
     })
 
+    // for edit buttons
     posts
         .map(post => document.getElementById(`comments-edit-${post.id}`))
         .forEach(btn => btn.addEventListener('click', (e) => {
             const id = Number(e.target.id.split('-')[2])
             onEditPost(id)
+        }))
+
+    // for delete buttons
+    posts
+        .map(post => document.getElementById(`comments-delete-${post.id}`))
+        .forEach(btn => btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            const id = Number(e.target.id.split('-')[2])
+            onDeletePost(id)
         }))
 }
 
@@ -106,6 +119,8 @@ const onFormSubmit = async (bpTitle, bpContent) => {
 }
 
 const onEditPost = (postId) => {
+    isEditing = true;
+    editedPostId = postId;
     // change view to form
     changeDisplay([formPage], [homePage], [navForm], [navHome])
 
@@ -117,6 +132,63 @@ const onEditPost = (postId) => {
     bpContentInput.value = blogPost.body;
 }
 
+const handleEditPost = async (bpTitle, bpContent) => {
+    // validate input
+    if (!bpTitle || !bpContent) {
+        changeDisplay([errorEl])
+        throw new Error('Form is invalid')
+    }
+
+    // find blog post
+    const bpBody = {
+        title: bpTitle,
+        body: bpContent
+    }
+
+    // PATCH blog post
+    const response = await fetch(`${baseUrl}/posts/${editedPostId}`, {
+        method: "PATCH",
+        body: JSON.stringify(bpBody),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        }
+    })
+    const { id, userId, title, body } = await response.json()
+    const updatedBlogPost = new BlogPost(id, userId, title, body)
+
+    const index = allPosts.findIndex(post => post.id === id)
+
+    allPosts[index] = updatedBlogPost;
+
+    // rerender blog posts
+    renderPosts(allPosts)
+
+    // cleanup
+    bpTitleInput.value = '';
+    bpContentInput.value = '';
+
+    // go back to home
+    changeDisplay([homePage], [formPage], [navHome], [navForm])
+}
+
+const onDeletePost = async (postId) => {
+    changeDisplay([loader], [bpContainer])
+    // delete post
+    await fetch(`${baseUrl}/posts/${postId}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        }
+    })
+
+    const index = allPosts.findIndex(post => post.id === postId)
+
+    allPosts.splice(index, 1)
+
+    renderPosts(allPosts)
+    changeDisplay([bpContainer], [loader])
+}
+
 // EVENT HANDLERS
 navForm.addEventListener('click', () => {
     changeDisplay([formPage], [homePage], [navForm], [navHome])
@@ -125,7 +197,13 @@ navHome.addEventListener('click', () => {
     changeDisplay([homePage], [formPage], [navHome], [navForm])
 })
 searchInput.addEventListener('input', (e) => onSearch(e.target.value))
-submitBtn.addEventListener('click', () => onFormSubmit(bpTitleInput.value, bpContentInput.value))
+submitBtn.addEventListener('click', () => {
+    if (isEditing) {
+        handleEditPost(bpTitleInput.value, bpContentInput.value)
+    } else {
+        onFormSubmit(bpTitleInput.value, bpContentInput.value)
+    }
+})
 
 // MODELS
 
